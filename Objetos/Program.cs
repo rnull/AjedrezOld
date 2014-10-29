d9d986540df03e41f3713d4d6efa1289aaaa8b15
Edgar Manuel García González <manolo161070@hotmail.com>
2014-10-29 08:30:09 -0600

    Cambio de Condiciones

--- Objetos/Program.cs
+++ Objetos/Program.cs
@@ -10,30 +10,26 @@ namespace Objetos
     {
         static void Main(string[] args)
         {
-            for (int a = 0; a <= 123454; a++)
-            {//Ciclado del programa
-                for (int k = 0; k <= 5342312; k++)
+            {
+                for (int k = 0; k <= 5342312; k++)//Ciclado del programa
                 {
                     // Modificar coordenadas A-H && 1-8
 
-                    Console.Write("Escribe tus coordenas [fila] [columna]:  ");
+                    Console.Write("\n Escribe tus coordenas [fila] [columna]: ");
                     string coordenadas = (Console.ReadLine());
 
                     char cf = coordenadas[0];
                     char cc = coordenadas[2];
                     int cf1 = 0;
 
-                    if (cf == 'A') { cf1 = 8; }
-                    else if (cf == 'a') { cf1 = 8; }
-                    else if (cf == 'B') { cf1 = 7; }
-                    else if (cf == 'b') { cf1 = 7; }
-                    else if (cf == 'C') { cf1 = 6; }
-                    else if (cf == 'c') { cf1 = 6; }
-                    else if (cf == 'D') { cf1 = 5; }
-                    else if (cf == 'E') { cf1 = 4; }
-                    else if (cf == 'F') { cf1 = 3; }
-                    else if (cf == 'G') { cf1 = 2; }
-                    else if (cf == 'H') { cf1 = 1; }
+                    if ((cf == 'A') || (cf == 'a')) { cf1 = 8; }
+                    else if ((cf == 'B') || (cf == 'b')) { cf1 = 7; }
+                    else if ((cf == 'C') || (cf == 'c')) { cf1 = 6; }
+                    else if ((cf == 'D') || (cf == 'd')) { cf1 = 5; }
+                    else if ((cf == 'E') || (cf == 'e')) { cf1 = 4; }
+                    else if ((cf == 'F') || (cf == 'f')) { cf1 = 3; }
+                    else if ((cf == 'G') || (cf == 'g')) { cf1 = 2; }
+                    else if ((cf == 'H') || (cf == 'h')) { cf1 = 1; }
                     else
                     {
                         Console.WriteLine("Movimiento no permitido");
@@ -59,8 +55,7 @@ namespace Objetos
 
                     Casilla temp = new Casilla(new Simbolo(' '));// = renglones[0].GetCasilla(0);
 
-                    string[] nuevoContenido = new string[4]{
-                "$$$$","$$$$","$$$$","$$$$"};
+                    string[] nuevoContenido = new string[4] { "$$$$", "$$$$", "$$$$", "$$$$" };
 
                     if (cf1 > 0)//Condición para denegar el movimiento si no esta dentro de los parametros
                     {
